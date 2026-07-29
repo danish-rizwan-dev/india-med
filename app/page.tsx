8a4ab6c4a4748b044a54fa95d737acb7b644ad40
Danish Rizwan <danishriz8287@gmail.com>
2026-07-30 01:26:47 +0530

    new redirect permaent

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-import { redirect } from "next/navigation";
+import { permanentRedirect } from "next/navigation";
 
 export default function Page() {
-  redirect("/en", 301);
+  permanentRedirect("/en");
 }
